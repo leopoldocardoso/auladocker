06a240f1cfdaa36b2fb594552e38bd173a0f8215
Leopoldo Cardoso <Leopoldo Cardoso>
2022-06-01 22:31:52 -0300

    Update de arquivos

---
+++ variaveis.tf
@@ -0,0 +1,38 @@
+#Uso de variáveis
+
+variable "namerg" {
+  type        = string
+  description = "Nome do Resource Group"
+  default     = "rg-proevolua"
+}
+
+variable "location" {
+  type        = string
+  default     = "northcentralus"
+  description = "Localizacao dos Recursos do Azure. Ex: northcentralus"
+}
+
+variable "tags" {
+  type        = map(any)
+  description = "Tags nos Recursos e Servicos do Azure"
+  default = {
+    Ambiente = "AulaDocker/Proevolua"
+    Aluno    = "Leopoldo Cardoso"
+  }
+}
+
+variable "vnetenderecos" {
+  type    = list(any)
+  default = ["10.0.0.0/16"]
+}
+
+variable "admin_username" {
+  type    = string
+  default = "leopoldo"
+}
+
+variable "admin_password" {
+  type    = string
+  default = "pcardoso@6801"
+}
+
