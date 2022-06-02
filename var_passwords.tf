caac364e065a1c23312b60c291d221c801140294
Leopoldo Cardoso <Leopoldo Cardoso>
2022-06-01 22:30:32 -0300

    Update de arquivos

---
+++ var_passwords.tf
@@ -0,0 +1,12 @@
+#Uso de variáveis na senha
+
+variable "admin_username" {
+  type    = string
+  default = "leopoldo"
+}
+
+variable "admin_password" {
+  type    = string
+  default = "pcardoso@6801"
+}
+
