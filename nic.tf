fb2066d9f48ab68221a5f765892a406e8df1384c
Leopoldo Cardoso <peixoto.leopoldo@outlook.com>
2022-06-10 22:40:21 -0300

    Adcionando nova interface de rede

--- nic.tf
+++ nic.tf
@@ -1,4 +1,4 @@
-# Deploy network interface vmsrvnginx
+# Deploy network interface vmauladocker
 resource "azurerm_network_interface" "nic" {
   name                = "nic-vm-auladocker"
   resource_group_name = var.namerg
@@ -8,6 +8,25 @@ resource "azurerm_network_interface" "nic" {
     subnet_id                     = azurerm_subnet.snet.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.pip.id
+  }
+
+  tags = {
+    Ambiente = "AulaDocker/Proevolua"
+    Aluno    = "Leopoldo Cardoso"
+  }
+
+}
+
+# Deploy network interface vmauladocker01
+resource "azurerm_network_interface" "nic01" {
+  name                = "nic-vm01-auladocker"
+  resource_group_name = var.namerg
+  location            = var.location
+  ip_configuration {
+    name                          = "nic-northcentralus-config"
+    subnet_id                     = azurerm_subnet.snet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip01.id
   }
 
   tags = {
